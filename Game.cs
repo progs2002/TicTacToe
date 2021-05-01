4bb17abc851772c11406a13455cb8b0e5a0366d4
progs2002 <39672908+progs2002@users.noreply.github.com>
2021-05-01 20:01:07 +0530

    tor maa ke chudi

--- Game.cs
+++ Game.cs
@@ -23,6 +23,9 @@ namespace Game
             {
                 for(j=0;j<=2;j++)
                     Console.Write("["+board[i,j]+"] ");
+                Console.Write("\t\t");
+                for(j=0;j<=2;j++)
+                    Console.Write("["+refe[i,j]+"] ");
                 Console.WriteLine();
             }
         }
@@ -33,8 +36,9 @@ namespace Game
             {
                 for(j=0;j<=2;j++)
                 {
-                    if(refe[i,j]==num)
+                    if(refe[i,j]==num && refe[i,j]!=0)
                     {
+                        
                         m=i;
                         n=j;
                         goto LoopEnd;
@@ -46,7 +50,72 @@ namespace Game
                 board[m,n]=ch;
             }
         }
-        Boolean endgame(char ch)
+        bool checkifdraw()
+        {
+            int i,j;
+            bool flag=true;
+            for(i=0;i<=2;i++)
+            {
+                for(j=0;j<=2;j++)
+                {
+                    if(board[i,j]==' ')
+                        flag = false;
+                }
+            }
+            return flag;
+            
+        }
+
+        bool predictdraw()
+        {
+            int i,j;
+            TicTac cloneboard = this;
+            for(i=0;i<=2;i++)
+            {
+                for(j=0;j<=2;j++)
+                {
+                    if(cloneboard.board[i,j]==' ')
+                    {
+                        cloneboard.check(refe[i,j],'X');
+                        if(cloneboard.endgame('X') == false && cloneboard.checkifdraw()==true)
+                            return true;
+                        
+                    }
+                }
+            }
+            return false;
+        }
+        bool predictdraw2()
+        {
+            int i,j,counter=0;
+            TicTac clone1 = this;
+            TicTac clone2 = this;
+            for(i=0;i<=2;i++)
+            {
+                for(j=0;j<=2;j++)
+                {
+                    if(clone1.board[i,j]==' ')
+                    {
+                        counter++;
+                        if(counter==1)
+                        {
+                            clone1.check(refe[i,j],'O');
+                            clone2.check(refe[i,j],'X');
+                        }
+                        if(counter==2)
+                        {
+                            clone1.check(refe[i,j],'X');
+                            clone2.check(refe[i,j],'O');
+                        }
+                    }
+                }
+            }
+            if((clone1.endgame('X') == false && clone1.checkifdraw()==true)&&(clone2.endgame('X') == false && clone2.checkifdraw()==true))
+                return true;
+            else
+                return false;
+        }
+        bool endgame(char ch)
         {
             int i;
             for(i=0;i<3;i++)
@@ -65,7 +134,7 @@ namespace Game
         static void Main(String[]args)
         {
             TicTac g1 = new TicTac();
-            int n;
+            int n,turn=0;
             
             startgame:
             g1.clearboard();
@@ -73,13 +142,23 @@ namespace Game
             Console.WriteLine("PLAYER 1 is X PLAYER 2 is O");
             while(true)
             {
+                if(turn== 8 && g1.predictdraw()==true)
+                {
+                    Console.WriteLine("GAME IS DRAWN");
+                    Console.ReadKey();
+                    break;
+                }
                 
                 Console.WriteLine("PLAYER 1 turn");
+                turn++;
                 n=Convert.ToInt32(Console.ReadLine());
                 if(n==0)
                     goto startgame;
                 g1.check(n,'X');
                 g1.displayboard();
+                
+                
+                    
                 if(g1.endgame('X') == true)
                 {
                     Console.WriteLine("Player 1 wins");
@@ -87,12 +166,23 @@ namespace Game
                     break;
                    
                 }
+                
+                
+                // if(turn== 7 && g1.predictdraw2()==true)
+                // {
+                //     Console.WriteLine("GAME IS DRAWN");
+                //     Console.ReadKey();
+                //     break;
+                // }
+
                 Console.WriteLine("PLAYER 2 turn");
+                turn++;
                 n=Convert.ToInt32(Console.ReadLine());
                 if(n==0)
                     goto startgame;
                 g1.check(n,'O');
                 g1.displayboard();
+                
                 if(g1.endgame('O') == true)
                 {
                     Console.WriteLine("Player 2 wins");
@@ -104,4 +194,3 @@ namespace Game
         }
     }
 }
-
